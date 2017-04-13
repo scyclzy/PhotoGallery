2146e626f67972bbbfbec163125c78d2c3ad2355
lizhongyuan <scyclzy@gmail.com>
2017-04-13 18:30:53 +0800

    get back to main thread from AsyncTask

--- src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,20 +1,21 @@
 package com.bignerdranch.android.photogallery;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 public class PhotoGalleryFragment extends Fragment {
 	private static final String TAG = "PhotoGalleryFragment";
 	GridView mGridView;
+	ArrayList<GalleryItem> mItems;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,16 +31,35 @@ public class PhotoGalleryFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 		
 		mGridView = (GridView)v.findViewById(R.id.gridView);
+		setupAdapter();
 		
 		return v;
 	}
 	
-	private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+	void setupAdapter() {
+		if( getActivity() == null || mGridView == null ) {
+			return ;
+		}
+		
+		if( mItems != null ) {
+			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), 
+					android.R.layout.simple_gallery_item, mItems));
+		} else {
+			mGridView.setAdapter(null);
+		}
+	}
+	
+	private class FetchItemsTask extends AsyncTask<Void, Void, ArrayList<GalleryItem>>{
 
 		@Override
-		protected Void doInBackground(Void... params) {
-			new FlickrFetchr().fetchItems();
-			return null;
+		protected ArrayList<GalleryItem> doInBackground(Void... params) {
+			return new FlickrFetchr().fetchItems();
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<GalleryItem> items) {
+			mItems = items;
+			setupAdapter();
 		}
 		
 	}
