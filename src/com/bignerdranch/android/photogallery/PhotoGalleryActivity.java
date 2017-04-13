035f63ee67ca55681f3c04d6818b03b5828d9399
lizhongyuan <scyclzy@gmail.com>
2017-04-13 10:10:46 +0800

    create app

--- src/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ src/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -1,36 +1,13 @@
 package com.bignerdranch.android.photogallery;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
 
-public class PhotoGalleryActivity extends ActionBarActivity {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_gallery);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.photo_gallery, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	protected Fragment createFragment() {
+		
+		return new PhotoGalleryFragment();
+	}
 }
---
+++ src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class PhotoGalleryFragment extends Fragment {
+	GridView mGridView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+		
+		mGridView = (GridView)v.findViewById(R.id.gridView);
+		
+		return v;
+	}
+
+}
