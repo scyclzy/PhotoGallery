4b14c5efcde66b71c45fdd6563a389063f5171e2
lizhongyuan <scyclzy@gmail.com>
2017-04-15 20:50:12 +0800

    use SearchView after Android 3.0

--- src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ src/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -6,8 +6,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -23,6 +28,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 public class PhotoGalleryFragment extends Fragment {
 	private static final String TAG = "PhotoGalleryFragment";
@@ -78,6 +84,19 @@ public class PhotoGalleryFragment extends Fragment {
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.fragment_photo_gallery, menu);
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// Pull out the SearchView
+			MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+			SearchView searchView = (SearchView)searchItem.getActionView();
+			
+			// Get the data from our searchable.xml as a SearchableInfo
+			SearchManager searchManager = (SearchManager)getActivity()
+					.getSystemService(Context.SEARCH_SERVICE);
+			ComponentName name = getActivity().getComponentName();
+			SearchableInfo searchInfo = searchManager.getSearchableInfo(name);
+			
+			searchView.setSearchableInfo(searchInfo);
+		}
 	}
 
 	@Override
