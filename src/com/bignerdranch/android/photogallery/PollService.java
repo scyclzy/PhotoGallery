7ee7af5a74ab90ca8e71af2ef80736a8e5ef9880
lizhongyuan <scyclzy@gmail.com>
2017-04-16 10:58:14 +0800

    function of service

--- src/com/bignerdranch/android/photogallery/PollService.java
+++ src/com/bignerdranch/android/photogallery/PollService.java
@@ -1,7 +1,9 @@
 package com.bignerdranch.android.photogallery;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 public class PollService extends IntentService {
@@ -13,6 +15,15 @@ public class PollService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
+		ConnectivityManager cm = (ConnectivityManager)
+				getSystemService(Context.CONNECTIVITY_SERVICE);
+		@SuppressWarnings("deprecation")
+		boolean isNetworkAvailable = cm.getBackgroundDataSetting() && 
+				cm.getActiveNetworkInfo() != null;
+		if(!isNetworkAvailable) {
+			return ;
+		}
+		
 		Log.i(TAG, "Received a intent: " + intent);
 	}
 
