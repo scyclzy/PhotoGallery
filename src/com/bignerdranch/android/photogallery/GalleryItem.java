ed14f3e17be918debcc079e2494a8842b9b6c544
lizhongyuan <scyclzy@gmail.com>
2017-04-17 20:00:40 +0800

    get photo page url

--- src/com/bignerdranch/android/photogallery/GalleryItem.java
+++ src/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -4,7 +4,20 @@ public class GalleryItem {
 	private String mCaption;
 	private String mId;
 	private String mUrl;
+	private String mOwner;
 	
+	public String getOwner() {
+		return mOwner;
+	}
+
+	public void setOwner(String owner) {
+		mOwner = owner;
+	}
+	
+	public String getPhotoPageUrl() {
+		return "https://www.flickr.com/photos/" + mOwner + "/" + mId;
+	}
+
 	public String toString() {
 		return mCaption;
 	}
